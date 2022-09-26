e0191a521bd60bc261b8222f630beb7c17175d20
Juan Menezes <99377147+JuanMLima@users.noreply.github.com>
2022-09-26 19:26:19 -0300

    Tabela de item

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ const App = () => {
 
       {/* Área de inserção */}
       
-      <TableArea/>
+      <TableArea list={filteredList} />
 
      </C.Body>
     </C.Container>
--- src/data/items.ts
+++ src/data/items.ts
@@ -1,8 +1,8 @@
 import { Item } from '../types/Item';
 export const items: Item[] = [
-    { date: new Date(2021, 9, 15), category: 'food', title: 'McDonalds', value: 32.12 },
-    { date: new Date(2021, 9, 15), category: 'food', title: 'Burger King', value: 28 },
-    { date: new Date(2021, 9, 16), category: 'rent', title: 'Aluguel apt', value: 2300 },
-    { date: new Date(2021, 9, 15), category: 'salary', title: 'Salário ACME', value: 32.12 },
+    { date: new Date(2022, 9, 26), category: 'food', title:'McDonalds', value: 32.12 },
+    { date: new Date(2022, 9, 26), category: 'food', title:'Burger King', value: 28 },
+    { date: new Date(2022, 10, 27), category: 'rent', title:'Aluguel apt', value: 2300 },
+    { date: new Date(2022, 8, 25), category: 'salary', title:'Salário ACME', value: 32.12 },
 
 ];
